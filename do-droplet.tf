6ccc3dae2f4eef4f079318fa02b2b33032fe9d7c
Aimeri Baddouh <aimeri.baddouh@serviceprosig.com>
2020-09-10 10:54:47 -0400

    Fix name of droplet

--- do-droplet.tf
+++ do-droplet.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_droplet" "do-droplet-1" {
   image = "ubuntu-20-04-x64"
-  name = "test"
+  name = "do-droplet-1"
   region = "nyc3"
   size = "s-1vcpu-1gb"
   private_networking = true
