41662e676200c860e6686179f3b3d8583bcfc868
Aimeri Baddouh <aimeri@baddouh.me>
2020-09-11 04:39:16 -0400

    refactor script

--- terraform-ansible.sh
+++ terraform-ansible.sh
@@ -17,13 +17,36 @@
 # I'm using my own image here, but you can build
 # your own from the Dockerfile in this repository.
 
-while [[ "$#" -gt 0 ]]; do
+start_terraform(){
+  docker run -it --rm -v `pwd`:/home/sloth \
+  aimeri/terraform-ansible terraform $@
+  if [[ $? -ne 0 ]]; then
+    exit 1;
+  fi;
+}
+start_ansible(){
+  docker run -it --rm -v `pwd`:/home/sloth \
+  -e ANSIBLE_HOST_KEY_CHECKING=False \
+  aimeri/terraform-ansible ansible $@
+  if [[ $? -ne 0 ]]; then
+    exit 1;
+  fi;
+}
+start_ansible_playbook(){
+  docker run -it --rm -v `pwd`:/home/sloth \
+  -e ANSIBLE_HOST_KEY_CHECKING=False \
+  aimeri/terraform-ansible ansible-playbook $@
+  if [[ $? -ne 0 ]]; then
+    exit 1;
+  fi;
+}
+
+if [[ "$#" -gt 0 ]]; then
     case $1 in
-        terraform) docker run -it --rm -v `pwd`:/home/sloth --env \
-                   TF_LOG=TRACE aimeri/terraform-ansible terraform ${@:2};;
-        ansible)   docker run -it --rm -v `pwd`:/home/sloth \
-                   aimeri/terraform-ansible ansible ${@:2};;
+        terraform)        start_terraform ${@:2};;
+        ansible)          start_ansible ${@:2};;
+        ansible-playbook) start_ansible_playbook ${@:2};;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
-done
+fi
